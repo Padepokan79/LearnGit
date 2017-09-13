bfcefc9d62f1c10e13a36d5183637fedef4289f5
Sharkoded <sharkoded@gmail.com>
2017-09-13 13:50:46 +0700

    add Database

---
+++ ManipulateDatabase/src/Select.java
@@ -0,0 +1,71 @@
+import java.sql.DriverManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.ResultSet;
+
+public class Select {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			// create our mysql database connection
+			
+			String myDriver = "org.gjt.mm.mysql.Driver";
+			String myUrl = "jdbc:mysql://192.168.2.192:3000/dbgajido";
+			Class.forName(myDriver);
+			Connection conn = (Connection) DriverManager.getConnection(myUrl, "taspen", "taspen");
+
+			// our SQL SELECT query. 
+			// if you only need a few columns, specify them by name instead of using "*"
+			String query = "SELECT * FROM fgaji limit 10;";
+			
+			PreparedStatement st = (PreparedStatement) conn.prepareStatement(query);
+			
+			// execute the query, and get a java resultset
+			ResultSet rs = (ResultSet) st.executeQuery();
+
+			System.out.println("------ ------ ------ ----- -----");
+			JSONArray arrayData = Convertor.convertToJSON(rs);
+			JSONObject obj =  arrayData.getJSONObject(0);
+			System.out.println(obj);
+			obj =  arrayData.getJSONObject(1);
+			System.out.println(obj);
+			obj =  arrayData.getJSONObject(2);
+			System.out.println(obj);
+
+
+//			System.out.println("********************************");
+//			System.out.println(arrayData.toString());
+//			System.out.println("++++++++++++++++++++++++++++++++");
+//			
+			System.out.println(Convertor.convertToXML(rs));
+			
+//			// iterate through the java resultset
+//			while (rs.next())
+//			{
+//				System.out.println(rs.getString(1));
+//				System.out.println(rs.getString(2));
+//				System.out.println(rs.getString(3));
+//				System.out.println(rs.getString(4));
+//				System.out.println(rs.getString(5));
+//				System.out.println("------ ------ ------ ----- -----");
+//				JSONArray arrayData = Convertor.convertToJSON(rs);
+//				System.out.println(arrayData.toString());
+//				System.out.println("++++++++++++++++++++++++++++++++");
+//				
+//			}
+
+			st.close();
+		}
+		catch (Exception e)
+		{
+			System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
