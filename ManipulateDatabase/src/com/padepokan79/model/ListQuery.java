f6c75083e65e2408704e8fc1221053dbac211ebd
Sharkoded <sharkoded@gmail.com>
2017-09-13 16:37:00 +0700

    add interface

---
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -0,0 +1,9 @@
+package com.padepokan79.model;
+
+public interface ListQuery {
+	public final String queryNamaDanLakiJomblo = 
+			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
+	public final String queryNamaDanStatusKawin = 
+			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
+	
+}
