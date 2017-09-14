056c6c70427c49ee037c3eb7938f6dff62fd67e4
selfimuji <selfimlestari@gmail.com>
2017-09-14 09:43:03 +0700

    add by selfionline10

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -7,7 +7,7 @@ public interface ListQuery {
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
 	public final String queryNipNamaGajiBersihDiAtas10Jt =
 			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000";
-	public final String queryJombloMasaKerja10thn = // add by selfi
+	public final String queryJombloMasaKerjaDiatas10thn = // add by selfi
 			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1";
 	public final String queryNamaJandaKembang = 
 			"SELECT nama,nip,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0;"; //add by siska
