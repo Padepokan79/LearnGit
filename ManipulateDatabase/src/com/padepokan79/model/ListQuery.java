fe278a490c104b3d83d17f3abfe6658bf6315ae8
unknown <siska23.teknik@gmail.com>
2017-09-14 09:44:34 +0700

    perbarui janda kembang

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -10,5 +10,5 @@ public interface ListQuery {
 	public final String queryNipNamaMasaKerjadiAtas10tahunBelumMenikah = // add by selfi
 			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1";
 	public final String queryNamaJandaKembang = 
-			"SELECT nama,nip,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0;"; //add by siska
+			"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 100;"; //add by siska
 }
