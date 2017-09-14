980485fad9c100eaeb6f4484c0de9d85cabcff72
selfimuji <selfimlestari@gmail.com>
2017-09-14 09:49:08 +0700

    tambah limit 100

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -6,9 +6,9 @@ public interface ListQuery {
 	public final String queryNamaDanStatusKawin = 
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
 	public final String queryNipNamaGajiBersihDiAtas10Jt =
-			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000";
+			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000 limit 100;";
 	public final String queryJombloMasaKerjaDiatas10thn = // add by selfi
-			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1";
+			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1 limit 100;";
 	public final String queryNamaJandaKembang = 
 			"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 100;"; //add by siska
 }
