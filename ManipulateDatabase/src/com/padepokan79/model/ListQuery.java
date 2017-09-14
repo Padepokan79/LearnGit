a19e4a223de858a1d3bcb3e9f54725d7ca217bcc
selfimuji <selfimlestari@gmail.com>
2017-09-14 09:20:32 +0700

    tambahqueryline10

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -7,6 +7,7 @@ public interface ListQuery {
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
 	public final String queryNipNamaGajiBersihDiAtas10Jt =
 			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000";
-	
+	public final String queryNipNamaMasaKerjadiAtas10tahunBelumMenikah =
+			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1";
 
 }
