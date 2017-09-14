2dfcffd3d0385f3410b1a5a514c2ca658b07c850
wandiniArdiansyah <wandz.ardz@gmail.com>
2017-09-14 09:53:34 +0700

    add gaji diatas 10jt

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -6,7 +6,6 @@ public interface ListQuery {
 	public final String queryNamaDanStatusKawin = 
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
 	public final String queryNipNamaGajiBersihDiAtas10Jt =
-			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000";
+			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000 limit 0,10;";
 	
-
 }
--- ManipulateDatabase/src/com/padepokan79/test/MainActivity.java
+++ ManipulateDatabase/src/com/padepokan79/test/MainActivity.java
@@ -13,18 +13,19 @@ public class MainActivity {
 		testNamaDanGajiBersih(); // add by ikan
 		testNamaDanStatusKawin();
 		testNamaDanLakiJomblo();
+		NipNamaGajiBersihDiAtas10Jt(); // add by wandi
 	}
-	
-	
-	
-	
+
+
+
+
 	public static void testNamaDanLakiJomblo(){
 		Fgaji fgaji = new Fgaji();
 		JSONArray data = fgaji.getNamaDanLakiJomblo();
 		//System.out.println(data);
 		showData(data,"nama","bersih");
 	} // ikan added test for NamaDanGajiBersih
-	
+
 	public static void testNamaDanGajiBersih(){
 		Fgaji fgaji = new Fgaji();
 		JSONArray data = fgaji.getNamaDanGajiBersih();
@@ -37,12 +38,19 @@ public class MainActivity {
 		//System.out.println(data);
 		showData(data,"nama","bersih");
 	} // ikan added test for testNamaDanStatusKawin
-	
+
+	public static void NipNamaGajiBersihDiAtas10Jt(){
+		Fgaji fgaji = new Fgaji();
+		JSONArray data = fgaji.getNipNamaGajiBersihDiAtas10Jt();
+		//System.out.println(data);
+		showData4Parameter(data,"nip", "nama", "bersih", "kdpangkat");
+	} // ikan added test for NamaDanGajiBersih
+
 	public static void showData(JSONArray arrayData,String fieldsa, String fieldsb) {
 		System.out.println(arrayData);
 		for (int i = 0; i < arrayData.length(); i++) {
 			JSONObject obj =  arrayData.getJSONObject(i);
-			System.out.println(obj);
+			//	System.out.println(obj);
 			String status = "";
 			if ((int)obj.get(fieldsb) == 1){
 				status = "Kawin";
@@ -53,5 +61,16 @@ public class MainActivity {
 		}
 	}
 
+// Test Runing by Wandi
+	public static void showData4Parameter(JSONArray arrayData,String fieldsa, String fieldsb, String fieldsc, String fieldsd) {
+		System.out.println(arrayData);
+		for (int i = 0; i < arrayData.length(); i++) {
+			JSONObject obj =  arrayData.getJSONObject(i);
+			//	System.out.println(obj);
+			System.out.println(obj.get(fieldsa)+ " "+ ": "+obj.get(fieldsb)+ " "+ ": "+obj.get(fieldsc)+ " "+ ": "+obj.get(fieldsd) );
+		}
+	}
+
 }
+
 
