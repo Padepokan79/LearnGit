c41c35bd1f057ed6d6013ebb95acf4e7cb2aeece
thoecoin <rismantoinformatika@gmail.com>
2017-09-14 10:48:21 +0700

    Get Updating Modeling

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -6,18 +6,9 @@ public interface ListQuery {
 	public final String queryNamaDanStatusKawin = 
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
 	public final String queryNipNamaGajiBersihDiAtas10Jt =
-<<<<<<< HEAD
-			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000 limit 100;";
-=======
 			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000 limit 0,10;";
->>>>>>> origin
 	public final String queryJombloMasaKerjaDiatas10thn = // add by selfi
 			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1 limit 0,10;";
 	public final String queryNamaJandaKembang = 
-<<<<<<< HEAD
-			"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 100;"; //add by siska
-=======
 			"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 0,10;"; //add by siska
->>>>>>> origin
-
 }
