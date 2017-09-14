c3e8a79fd044489be88d8fb87cd7e8ae80fe3684
rzkypprtm <rzkypprtm@yahoo.com>
2017-09-14 10:50:12 +0700

    query tabel 1 final v.1

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -5,11 +5,13 @@ public interface ListQuery {
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
 	public final String queryNamaDanStatusKawin = 
 			"SELECT nama,bersih FROM fgaji where kdjenkel=1 limit 0,10;";
-	public final String queryNipNamaGajiBersihDiAtas10Jt =
+	public final String queryNipNamaGajiBersihDiAtas10Jt = // add by rzkypprtm
 			"select nip, nama, bersih, kdpangkat from fgaji where bersih > 10000000 limit 0,10;";
 	public final String queryJombloMasaKerjaDiatas10thn = // add by selfi
 			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=1 limit 0,10;";
 	public final String queryNamaJandaKembang = 
 			"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 0,10;"; //add by siska
+	public final String queryNamaUsiaAntara25Sampai35 = //add by rzkypprtm
+			"SELECT nip, nama, bersih, kdpangkat, TGLLHR as usia from fgaji where kdstawin = '1' and TGLLHR <= '1992/09/14' and TGLLHR >= '1982/09/14' limit 0,10";
 
 }
