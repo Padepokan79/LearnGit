95c4afa784cd5aff9020cf30ebef0601585d7b17
muamarrrrr <muammar.rijalajah@gmail.com>
2017-09-15 08:30:25 +0700

    ak

--- ManipulateDatabase/src/com/padepokan79/test/MainActivity.java
+++ ManipulateDatabase/src/com/padepokan79/test/MainActivity.java
@@ -1,54 +1,33 @@
 package com.padepokan79.test;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import com.padepokan79.model.Fgaji;
-
+import java.util.Scanner;
 public class MainActivity {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		testNamaDanGajiBersih(); // add by ikan
-		testNamaDanStatusKawin();
-		testNamaDanLakiJomblo();
+		menuUtama();
 	}
+	// Editing By Sandi
 	
 	
-	
-	
-	public static void testNamaDanLakiJomblo(){
-		Fgaji fgaji = new Fgaji();
-		JSONArray data = fgaji.getNamaDanLakiJomblo();
-		//System.out.println(data);
-		showData(data,"nama","bersih");
-	} // ikan added test for NamaDanGajiBersih
-	
-	public static void testNamaDanGajiBersih(){
-		Fgaji fgaji = new Fgaji();
-		JSONArray data = fgaji.getNamaDanGajiBersih();
-		//System.out.println(data);
-		showData(data,"nama","bersih");
-	} // ikan added test for NamaDanGajiBersih
-	public static void testNamaDanStatusKawin(){
-		Fgaji fgaji = new Fgaji();
-		JSONArray data = fgaji.getNamaDanStatusKawin();
-		//System.out.println(data);
-		showData(data,"nama","kdstawin");
-	} // ikan added test for testNamaDanStatusKawin
-	
-	public static void showData(JSONArray arrayData,String fieldsa, String fieldsb) {
-		System.out.println(arrayData);
-		for (int i = 0; i < arrayData.length(); i++) {
-			JSONObject obj =  arrayData.getJSONObject(i);
-			System.out.println(obj);
-			String status = "";
-			if ((int)obj.get(fieldsb) == 1){
-				status = "Kawin";
-			}else{
-				status = "Tidak kawin";
-			}
-			System.out.println(obj.get(fieldsa)+ " "+ ": "+status );
+	public static void menuUtama() {
+		int input;
+		FGajiMenu newGaji = new FGajiMenu();
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("+--------------------------------------------------------------------+");
+		System.out.println("|                            Menu Utama                              |");
+		System.out.println("+--------------------------------------------------------------------+");
+		System.out.println("|    1. Pegawai    |    2. Gaji    |  3. Tunjangan  |    4. Jabatan  |");
+		System.out.println("+--------------------------------------------------------------------+");
+		System.out.print("Pilihan : ");
+		input = in.nextInt();
+		
+		if (input==1) {
+			newGaji.mainMenu();
+		}else {
+			System.out.println("Maaf, inputan anda salah");
+			menuUtama();
 		}
 	}
 
---
+++ ManipulateDatabase/src/com/padepokan79/test/MainTest.java
@@ -0,0 +1,40 @@
+package com.padepokan79.test;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.padepokan79.model.Fgaji;
+
+public class MainTest {
+	public static void main(String[]args) {
+		getNamaJandaKembang();
+	}
+	//"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 0,10;"
+	public static void getNamaJandaKembang(){
+		Fgaji fgaji = new Fgaji();
+		JSONArray data = fgaji.getNamaJandaKembang();
+		//System.out.println(data);
+		showData4Parameter(data,"nip", "nama", "kdstawin", "janak");
+	} // ikan added test for NamaDanGajiBersih
+	public static void showData4Parameter(JSONArray arrayData,String fieldsa, String fieldsb, String fieldsc, String fieldsd) {
+		//System.out.println(arrayData);
+		System.out.println("+---------------------------------------------------------------------------------+");
+		System.out.println("|   NIP            |        Nama              | Status Perkawinan  |  Jumlah Anak |");
+		System.out.println("+---------------------------------------------------------------------------------+");
+		String space;
+	
+		for (int i = 0; i < arrayData.length(); i++) {
+			JSONObject obj =  arrayData.getJSONObject(i);	
+			//	System.out.println(obj);
+			space=(String) obj.get(fieldsb);
+			System.out.print("|"+obj.get(fieldsa));		
+			space=space.trim();
+			System.out.print("|"+space);
+			
+			for (int j = 0; j  <26-(space.length()); j++) {
+				System.out.print(" ");}
+			System.out.print("|           "+obj.get(fieldsc));
+			System.out.println("        |       "+obj.get(fieldsd)+"      |");
+			System.out.println("+---------------------------------------------------------------------------------+");
+		}
+	}
+}
