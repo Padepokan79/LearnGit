54608f2885f819f0477b1a5ae6beff3c30a19f25
unknown <siska23.teknik@gmail.com>
2017-09-15 09:25:29 +0700

    add jomblo wafat

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -60,6 +60,13 @@ public interface ListQuery {
 			"SELECT hutang.NIP, mstpegawai.nama, hutang.JMLHUTANG AS JUMLAH_HUTANG from hutang,mstpegawai WHERE hutang.NIP=mstpegawai.NIP order by JUMLAH_HUTANG DESC LIMIT 10;";
 	public final String queryHutangPalingbanyak = // add by selfi
 	"select NIP, JMLHUTANG,JMLBULAN from hutang ORDER BY JMLBULAN AND JMLHUTANG ASC limit 10;"; 
-			
+
+	
+//===========================================================================================================================================
+//tabel 5 keluarga
+	public final String queryPNSJombloWafat =
+			"select nip,nmkel as Nama,tglnikah,tglwafat from keluarga where tglnikah IS NULL and tglwafat IS NOT NULL limit 10;"; //add by siska
+
+
 	
 }
