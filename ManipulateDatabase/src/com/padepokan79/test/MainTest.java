9ef3b0b1088a4c1854360675e15a98db2f3925c9
uciha1ndr4 <indranofiyan@gmail.com>
2017-09-15 10:38:13 +0700

    test

--- ManipulateDatabase/src/com/padepokan79/test/MainTest.java
+++ ManipulateDatabase/src/com/padepokan79/test/MainTest.java
@@ -5,20 +5,20 @@ import com.padepokan79.model.Twafat;
 
 public class MainTest {
 	public static void main(String[]args) {
-		getqueryPenerbitBukanDariLurah();
+		getqueryPNSYangMatinyaPalingLama();
 	}
 	//"SELECT nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 0,10;"
-	public static void getqueryPenerbitBukanDariLurah(){
+	public static void getqueryPNSYangMatinyaPalingLama(){
 		Twafat twafat = new Twafat();
-		JSONArray data = twafat.getqueryPenerbitBukanDariLurah();
+		JSONArray data = twafat.getqueryPNSYangMatinyaPalingLama();
 		//System.out.println(data);
-		showData3Parameter(data,"nip", "nama", "penerbit");
+		showData3Parameter(data,"nip", "nama", "tglwafat");
 	} // ikan added test for NamaDanGajiBersih
 	public static void showData3Parameter(JSONArray arrayData,String fielda, String fieldb, String fieldc) {
 		//System.out.println(arrayData);
-		System.out.println("+-----------------------------------------------------------------------------------------------------------+");
-		System.out.println("|          NIP           |                  Nama                   |               Penerbit                 |");
-		System.out.println("+-----------------------------------------------------------------------------------------------------------+");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("|          NIP           |                  Nama                   |  Tanggal wafat  |");
+		System.out.println("+------------------------------------------------------------------------------------+");
 		String space;
 		
 		for (int i = 0; i < arrayData.length(); i++) {
@@ -37,14 +37,12 @@ public class MainTest {
 			for (int j = 0; j  <35-(space.length()); j++) {
 				System.out.print(" ");
 			}
-			
-			space=(String) obj.get(fieldc);
-			space = space.trim();
-			System.out.print("     | "+space);
-			for (int j = 0; j  <39-(space.length()); j++) {
+			System.out.print("     | ");
+			System.out.print(obj.get(fieldc));
+			for (int j = 0; j  <21-(space.length()); j++) {
 				System.out.print(" ");
 			}
-			
+			//
 //			space=(String) obj.get(fieldd);
 //			space = space.trim();
 //			System.out.print("    | "+space);
@@ -59,7 +57,7 @@ public class MainTest {
 //			}
 			
 			System.out.println("|");
-			System.out.println("+-----------------------------------------------------------------------------------------------------------+");
+			System.out.println("+------------------------------------------------------------------------------------+");
 		}
 	}	
 }
