b27a0d2ac30b487edc96ea3ab0830216cb83c975
unknown <siska23.teknik@gmail.com>
2017-09-15 13:23:34 +0700

    refrsh

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -77,6 +77,7 @@ public interface ListQuery {
 	public final String queryNamaKeluargaPNSyangPernahMenikahLaluCerai = // add by rzkypprtm
 			"SELECT NIP, NMKEL as Nama, TGLNIKAH, TGLCERAI FROM keluarga WHERE TGLNIKAH IS NOT NULL AND TGLCERAI IS NOT NULL;";
 
+	
 //===========================================================================================================================================
 //tabel 6 skpp_pegawai
 
