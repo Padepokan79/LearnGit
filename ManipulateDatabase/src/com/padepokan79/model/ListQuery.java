fa39632f45cb21dd92fe1dda600e4315758383a7
uciha1ndr4 <indranofiyan@gmail.com>
2017-09-15 11:22:56 +0700

    add indra Satker

--- ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
+++ ManipulateDatabase/src/com/padepokan79/model/ListQuery.java
@@ -10,42 +10,15 @@ public interface ListQuery {
 	public final String queryJombloMasaKerjaDiatas10thn = // add by selfi
 			"select NIP, NAMA, MASKER, KDSTAWIN FROM fgaji where MASKER > 10 and KDSTAWIN=0 limit 0,10;";
 	public final String queryNamaJandaKembang = 
-
-			"SELECT DISTINCT nip,nama,kdjenkel,kdstawin,janak FROM fgaji where kdjenkel=2 and kdstawin=3 and janak=0 limit 100;"; //add by siska
+			"SELECT distinct nip,nama,kdstawin,janak FROM fgaji where kdstawin=3 and janak=0 limit 0,10;"; //add by siska
 	public final String queryNamaUsiaAntara25Sampai35 = //add by rzkypprtm
 			"SELECT nip, nama, bersih, kdpangkat, TGLLHR as usia from fgaji where kdstawin = '1' and TGLLHR <= '1992/09/14' and TGLLHR >= '1982/09/14' limit 0,10;";
-
 	public final String queryNamaSuperDad = 
 			"SELECT nip,nama,kdjenkel,tgllhr,kdpangkat,masker,janak FROM fgaji where kdjenkel = 1 and kdpangkat=1  and masker > 20 and janak > 1 limit 0,10;"; //add by siska
 
 
 //========================================================================================================================================
 // tabel 2 SATKERJA
-<<<<<<< HEAD
-	
-	public final String queryNamaAtasanDRSDanJabatanCamat =  // tambahan selfi
-			"SELECT SATKERJA,JAB_ATASAN,DESCSATKER,KOTA, NAMA_ATASA AS NAMA_ATASAN FROM satkerja WHERE NAMA_ATASA LIKE 'DRS%' AND JAB_ATASAN LIKE 'CAMAT%' ";
-	public final String queryKepalaDinasHaji = 
-			"select SATKERJA,descsatker,nama_atasa,jab_atasan from satkerja where descsatker like '%dinas%' and nama_atasa like 'h%' ;"; //add by siska	
-	public final String queryBendaharaPembantuDaftarGaji = 
-			"select SATKERJA,descsatker,nip_bend,nama_bend,jab_bend,jab_operat from satkerja where jab_bend like '%pembantu%' and jab_operat like '%gaji%';"; //add by siska	
-	public final String queryNamaOperatorDenganAkhiranH = // add by rzkypprtm
-			"SELECT SATKERJA as Satuan_Kerja, DESCSATKER as Deskripsi, Kota, nama_opera as Nama_Operator from satkerja where nama_opera like '%h'";
-	public final String queryJumlahPegawaiYangKerjaDiDinasKesehatan = // add by rzkypprtm
-			"SELECT DESCSATKER as Satuan_Kerja, COUNT(*) from satkerja where DESCSATKER like '%PUSKESMAS%' or DESCSATKER like '%KESEHATAN%' GROUP BY DESCSATKER LIMIT 0,10";
-
-
-
-//===========================================================================================================================================
-//tabel 3 twafat
-
-	public final String queryNIPNamaTanggalWafat = // add by rzkypprtm
-			"SELECT twafat.NIP,mstpegawai.nama,twafat.TGLWAFAT FROM twafat,mstpegawai WHERE twafat.NIP = mstpegawai.NIP AND twafat.TGLWAFAT > '2014/04/31'";
-	public final String queryKdKubKelPenerimaKosong =
-			"select twafat.NIP,mstpegawai.nama,twafat.TGLWAFAT,penerbit,kdhubkelpenerima from  twafat,mstpegawai where twafat.NIP = mstpegawai.NIP AND twafat.TGLWAFAT and kdhubkelpenerima=''  limit 10;"; //add by siska
-	public final String queryPenerbitBukanDariLurah = // add by selfi
-			"select twafat.NIP,mstpegawai.nama, twafat.PENERBIT from twafat,mstpegawai where twafat.NIP=mstpegawai.NIP AND NOT PENERBIT LIKE '%lurah%' limit 10;";
-=======
 	
 	public final String queryNamaAtasanDRSDanJabatanCamat =  // tambahan selfi
 			"SELECT SATKERJA,JAB_ATASAN,DESCSATKER,KOTA, NAMA_ATASA AS NAMA_ATASAN FROM satkerja WHERE NAMA_ATASA LIKE 'DRS%' AND JAB_ATASAN LIKE 'CAMAT%' ;";
@@ -87,7 +60,22 @@ public interface ListQuery {
 			"SELECT hutang.NIP, mstpegawai.nama, hutang.JMLHUTANG AS JUMLAH_HUTANG from hutang,mstpegawai WHERE hutang.NIP=mstpegawai.NIP order by JUMLAH_HUTANG DESC LIMIT 10;";
 	public final String queryHutangPalingbanyak = // add by selfi
 	"select NIP, JMLHUTANG,JMLBULAN from hutang ORDER BY JMLBULAN AND JMLHUTANG ASC limit 10;"; 
-			
+
 	
->>>>>>> origin/Controling
+//===========================================================================================================================================
+//tabel 5 keluarga
+	
+	public final String queryPNSJombloWafat =
+			"select nip,nmkel as Nama,tglnikah,tglwafat from keluarga where tglnikah IS NULL and tglwafat IS NOT NULL limit 10;"; //add by siska
+	public final String queryPNSUsiaPernikahanPerak25tahun = // add by selfi
+			"select NIP, NMKEL as NAMAKELUARGA,TGLNIKAH from keluarga where TGLNIKAH > '1992-01-01' AND TGLNIKAH < '1992-12-30';";
+	public final String queryPNSjomblosejati = // add by nopan
+			"select NMKEL,TGLLHR,TGLNIKAH from keluarga where TGLLHR <= '1987/09/15' AND TGLNIKAH IS NULL;";
+	public final String queryKeluargaPNSyangSudahMenikahDanMempunyaiKartuSuamiIstri = // add by rzkypprtm
+			"SELECT NIP, NMKEL as Nama, TGLNIKAH, NIPSUAMIISTRI FROM keluarga WHERE TGLNIKAH IS NOT NULL AND NIPSUAMIISTRI IS NOT NULL AND NOT NIPSUAMIISTRI = '';";
+	public final String queryNamaKeluargaPNSyangPernahMenikahLaluCerai = // add by rzkypprtm
+			"SELECT NIP, NMKEL as Nama, TGLNIKAH, TGLCERAI FROM keluarga WHERE TGLNIKAH IS NOT NULL AND TGLCERAI IS NOT NULL;";
+	
 }
+
+
