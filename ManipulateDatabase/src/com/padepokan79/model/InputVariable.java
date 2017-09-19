82ce87764defa68f07937452f99dedae95c34862
wandiniArdiansyah <wandz.ardz@gmail.com>
2017-09-19 13:37:50 +0700

    update type data inputVariable dan Umum

--- ManipulateDatabase/src/com/padepokan79/model/InputVariable.java
+++ ManipulateDatabase/src/com/padepokan79/model/InputVariable.java
@@ -46,6 +46,7 @@ public class InputVariable {
 	
 	//getkode
 	public static int inputKode;   // ? 1. untuk kode agama, trans, skpd, gol
+	public static String inputGolongan;
 	
 	//Tabel 15 t_tunjangan
 	public static int inputJumlahAwal;
